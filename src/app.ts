05cbd4d63de3908fec797e8254708a10b4f347a5
Shakil Ahmed <sa3akash@gmail.com>
2025-11-12 19:42:20 +0600

    Enhance Quran API with compression, security, and rate limiting features; refactor QuranController for improved caching and file handling

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,71 @@
-import express from 'express';
-import "dotenv/config"
-import { config } from './config';
-import { mainRouter } from './routes';
-import { globalErrorHandler } from 'error-express';
+import express from "express";
+import "dotenv/config";
+import { config } from "./config";
+import { mainRouter } from "./routes";
+import { globalErrorHandler } from "error-express";
+import comprassion from "compression";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10MB" }));
+app.use(express.urlencoded({ extended: true, limit: "10MB" }));
+app.use(comprassion());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      useDefaults: true,
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "blob:"],
+        connectSrc: ["'self'", "https:"],
+        fontSrc: ["'self'", "https:", "data:"],
+        objectSrc: ["'none'"],
+        baseUri: ["'self'"],
+        frameAncestors: ["'none'"],
+      },
+    },
+    crossOriginEmbedderPolicy: true,
+    crossOriginOpenerPolicy: {
+      policy: "same-origin",
+    },
+    crossOriginResourcePolicy: {
+      policy: "same-origin",
+    },
+    dnsPrefetchControl: {
+      allow: false,
+    },
+    frameguard: {
+      action: "deny",
+    },
+    hidePoweredBy: true,
+    hsts: {
+      maxAge: 63072000,
+      includeSubDomains: true,
+      preload: true,
+    },
+    ieNoOpen: true,
+    noSniff: true,
+    originAgentCluster: true,
+    permittedCrossDomainPolicies: {
+      permittedPolicies: "none",
+    },
+    referrerPolicy: {
+      policy: "no-referrer",
+    },
+    xssFilter: true,
+  })
+);
+app.use(
+  rateLimit({
+    windowMs: 60 * 100,
+    limit: 50,
+    standardHeaders: "draft-7",
+    legacyHeaders: false,
+  })
+);
 
 // routus
 mainRouter(app);
@@ -15,5 +73,5 @@ mainRouter(app);
 app.use(globalErrorHandler);
 
 app.listen(config.PORT, () => {
-    console.log(`Server is running on port http://localhost:${config.PORT}`);
+  console.log(`Server is running on port http://localhost:${config.PORT}`);
 });
